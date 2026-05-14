c85d0466c622bef9b974858de6e6c288fa78be91
Irell Zane <irellzane@tecnico.ulisboa.pt>
2026-05-14 11:48:42 +0100

    Initial Commit

---
+++ Voco/VocoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocoApp.swift
+//  Voco
+//
+//  Created by Irell Zane on 14/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct VocoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
