7578cecdde25246fbb13b7159598cb6f5d164e0b
Irell Zane <itsirellzane@gmail.com>
2026-06-08 14:05:24 +0100

    feat: clear error messages for LlamaError
    - couldNotInitializeContext: 'Not enough memory to load the model. Close other apps and try again.'
    - contextSizeLimitExeeded: 'Input text is too long for the model to process.'
    - decodingError: 'The model encountered an error during generation. Try again.'
    - emptyMessageArray: 'No input provided to the model.'
    - Added LocalizedError conformance with Foundation import

--- Packages/swift-llama-cpp/Sources/SwiftLlama/LlamaError.swift
+++ Packages/swift-llama-cpp/Sources/SwiftLlama/LlamaError.swift
@@ -5,9 +5,24 @@
 //  Created by Piotr Gorzelany on 30/07/2025.
 //
 
-public enum LlamaError: Error{
+import Foundation
+
+public enum LlamaError: Error, LocalizedError {
     case couldNotInitializeContext
     case contextSizeLimitExeeded
     case decodingError
     case emptyMessageArray
+
+    public var errorDescription: String? {
+        switch self {
+        case .couldNotInitializeContext:
+            return "Not enough memory to load the model. Close other apps and try again."
+        case .contextSizeLimitExeeded:
+            return "Input text is too long for the model to process."
+        case .decodingError:
+            return "The model encountered an error during generation. Try again."
+        case .emptyMessageArray:
+            return "No input provided to the model."
+        }
+    }
 }
